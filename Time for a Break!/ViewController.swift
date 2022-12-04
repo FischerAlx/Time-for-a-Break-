5fac7a2cfc80713e531e9e9b027127749a3a5028
FischerAlx <fisheralx@icloud.com>
2022-12-04 19:44:39 +0100

    Initial Commit

---
+++ Time for a Break!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Time for a Break!
+//
+//  Created by Александр Фишер on 04.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
