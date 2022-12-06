fcc36b98862100260217e91d543d34d82d441040
FischerAlx <fisheralx@icloud.com>
2022-12-06 20:42:46 +0100

    add a lot of new
    progress Bar, timer and clean code

--- Time for a Break!/ViewController.swift
+++ Time for a Break!/ViewController.swift
@@ -9,14 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let freeTime = ["Тест": 5, "Минута": 60, "Час": 3600]
+    var secondsLeft = 5
+    var timer = Timer()
     
+    @IBOutlet weak var progressBarVIew: UIProgressView!
     @IBAction func pressedSelectedTime(_ sender: UIButton) {
-        if sender.currentTitle != nil {
-            print(sender.currentTitle)
-        } else {
-            print("nilllll((((")
-        }
+        
+        timer.invalidate()
+        secondsLeft = freeTime[(sender.titleLabel?.text!)!]!
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
     }
+    
+        @objc func updateTimer() {
+            var progressBarCount: Float = 1.0
+            if secondsLeft > 0 {
+                
+                //print("\(secondsLeft) seconds")
+                progressBarCount = Float(1/secondsLeft)
+                progressBarVIew.progress = progressBarCount
+                secondsLeft -= 1
+                
+            } else {
+                print("end")
+            }
+        }
+        
+        
+    
     
     
     
